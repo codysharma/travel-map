9d9df3d856a83a87493cecb20f53bfeda363f4cb
codysharma-ga <codysharmageneralassembly@gmail.com>
2024-02-21 19:25:14 -0700

    trying different map-gl technique

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-import {useState} from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import ReactMapGL from 'react-map-gl';
 
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29keXNoYXJtYSIsImEiOiJjbHM2aTB4YW4wd3c1MnBucmZwb3R0aWdpIn0.2-nmMKx09tjnsgD9QDLFTQ';
+// Change to pull from .env after testing
 
 function App() {
   
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY29keXNoYXJtYSIsImEiOiJjbHM2aTB4YW4wd3c1MnBucmZwb3R0aWdpIn0.2-nmMKx09tjnsgD9QDLFTQ';
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-  });
+  const mapContainer = useRef(null);
+
+  const [newPlace, setNewPlace] = useState(null)
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current, 
+      style: 'mapbox://styles/mapbox/streets-v11', 
+      center: [-105, 39.7], // starting position [longitude, latitude]
+      zoom: 3,
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -21,10 +29,7 @@ function App() {
       </header>
       <p>body</p>
       <h1>Travel Map</h1>
-      <ReactMapGl 
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      />
+      <div ref={mapContainer} className="map-container" />
     </div>
   );
 }
